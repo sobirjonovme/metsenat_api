fbd67e89c7530bf89f14a73132398c78766e8e37
sobirjonov_me <huntsman6004@gmail.com>
2022-10-01 15:13:19 +0500

    sponsorship.signals changed bugs fixed

--- sponsorship/signals.py
+++ sponsorship/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 
 from .models import SponsorStudent
 
@@ -7,7 +7,6 @@ from .models import SponsorStudent
 @receiver(post_save, sender=SponsorStudent)
 def sponsorstudent(sender, instance, created, **kwargs):
     if created:
-        print("\n\n Create signal\n\n")
         sponsor = instance.sponsor
         sponsor.spent_money += instance.amount
         sponsor.save()
@@ -15,3 +14,31 @@ def sponsorstudent(sender, instance, created, **kwargs):
         student = instance.student
         student.received_money += instance.amount
         student.save()
+
+    else:
+        sponsor = instance.sponsor
+        sponsorships = SponsorStudent.objects.filter(sponsor=sponsor)
+        money = 0
+        for i in range(len(sponsorships)):
+            money += sponsorships[i].amount
+        sponsor.spent_money = money
+        sponsor.save()
+
+        student = instance.student
+        sponsorships = SponsorStudent.objects.filter(student=student)
+        money = 0
+        for i in range(len(sponsorships)):
+            money += sponsorships[i].amount
+        student.received_money = money
+        student.save()
+
+
+@receiver(pre_delete, sender=SponsorStudent)
+def delete_profile(sender, instance, *args, **kwargs):
+    sponsor = instance.sponsor
+    sponsor.spent_money -= instance.amount
+    sponsor.save()
+
+    student = instance.student
+    student.received_money -= instance.amount
+    student.save()
