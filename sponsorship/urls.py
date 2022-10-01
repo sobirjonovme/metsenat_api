665d1df9143250719cccd7b7f66f81211c22478b
sobirjonov_me <huntsman6004@gmail.com>
2022-10-01 23:23:03 +0500

    some cahnges urls

--- sponsorship/urls.py
+++ sponsorship/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('', SponsorStudentListAPIView.as_view(), name='list'),
     path('create/', CreateSponsorStudentAPIView.as_view(), name='create'),
     path('<int:pk>/', SponsorStudentAPIView.as_view(), name='detail'),
-    path('<int:pk>/update', UpdateSponsorStudentAPIView.as_view(), name='update'),
+    path('<int:pk>/update/', UpdateSponsorStudentAPIView.as_view(), name='update'),
 
     path('dashboard/overall-statistics/', OverallStatistics.as_view(), name='dashboard-overall'),
     path('dashboard/sponsors/', SponsorStatistics.as_view(), name='dashboard-sponsors'),
